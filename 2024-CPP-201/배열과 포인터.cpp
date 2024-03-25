abb3530c4e47630cb32ab9fa337fa26ea2ecb69a
anju0210 <s2309@e-mirim.hs.kr>
2024-03-25 14:59:21 +0900

    배열 원소 사이의 주소값 차이

---
+++ 2024-CPP-201/배열과 포인터.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int arr[4];
+	char str[4];
+	
+	// 4바이트 차이(x86으로 설정하면 더 좋아요)
+	printf("%d %d\n", &arr, &arr[1]);
+	// 1바이트 차이
+	printf("%d %d\n", &str, &str[1]);
+}
