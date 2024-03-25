1485eedb99267ad5117a3d5e29461dbaf853ca8b
anju0210 <s2309@e-mirim.hs.kr>
2024-03-25 15:02:55 +0900

    &arr[1]과 arr+1은 같다

--- 2024-CPP-201/배열과 포인터.cpp
+++ 2024-CPP-201/배열과 포인터.cpp
@@ -5,7 +5,7 @@ int main(void) {
 	char str[4];
 	
 	// 4바이트 차이(x86으로 설정하면 더 좋아요)
-	printf("%d %d\n", &arr, &arr[1]);
-	// 1바이트 차이
-	printf("%d %d\n", &str, &str[1]);
+	printf("%d %d\n", &arr[0], &arr[1]);
+	// 위의 코드와 결과가 같다
+	printf("%d %d\n", arr, arr+1);
 }
