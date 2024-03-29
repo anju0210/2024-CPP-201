1d4175a110e41d425096135baa60a075341df8e1
anju0210 <s2309@e-mirim.hs.kr>
2024-03-29 09:08:03 +0900

    배열과 포인터

--- 2024-CPP-201/배열과 포인터.cpp
+++ 2024-CPP-201/배열과 포인터.cpp
@@ -3,10 +3,16 @@
 int main(void) {
 	int arr[4] = { 10, 20, 30, 40 };
 	char str[4];
+
+	int* pi = arr;
+	char* pc = str;
 	
 	// 두 코드의 결과는 같다
-	printf("%d %d\n", *(& arr[0]), *(& arr[1]));
-	printf("%d %d\n", *(arr), *(arr+1));
+	printf("%d %d\n", & arr[0], & arr[1]);
+	printf("%d %d\n\n", arr, arr+1);
+
+	printf("%d %d\n", pi,  pi+ 1);	// 4차이
+	printf("%d %d\n", pc, pc + 1);	// 1차이
 
 	// &를 쓰는 연산은 참조연산
 	// *를 쓰는 연산은 역참조연산
