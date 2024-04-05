71992703eddfa8309d3897e0eac236ba157b1757
anju0210 <s2309@e-mirim.hs.kr>
2024-04-05 09:46:16 +0900

    주영 문자열 대입(복사)-C스타일2

--- 2024-CPP-201/02-03 문자열 대입(복사).cpp
+++ 2024-CPP-201/02-03 문자열 대입(복사).cpp
@@ -1,12 +1,11 @@
 ﻿#include <stdio.h>
+#include <string.h>
 
 void main(void) {
 	char str[100] = "Juyoung";
 	char str2[100];
 
-	int i = 0;
-	for (int i=0; str[i-1]!='\0'; i++)
-		str2[i] = str[i];
+	strcpy(str2, str);
 
 	printf("%s", str);
 	// str에 있는 내용을 str2에 복사하기
