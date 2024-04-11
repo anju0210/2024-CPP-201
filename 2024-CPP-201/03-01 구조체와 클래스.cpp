e0293876876f0a9fe4871ed8a671f248b2813c9a
anju0210 <s2309@e-mirim.hs.kr>
2024-04-12 08:54:56 +0900

    생성자 오버로딩

--- 2024-CPP-201/03-01 구조체와 클래스.cpp
+++ 2024-CPP-201/03-01 구조체와 클래스.cpp
@@ -4,12 +4,18 @@ using namespace std;
 
 // 클래스는 default가 private
 class Student {
+public:
 	// 생성자 : 객체를 생성할 때 호출되는 함수
 	// 반환형이 없다
-public:
 	Student(int hakbun, string name) {
 		hakbun_ = hakbun;
 		name_ = name;
+	}
+
+	// 생성자 오버로딩
+	Student() {
+		hakbun_ = 21;
+		name_ = "JWP";
 	}
 
 private :
@@ -22,5 +28,6 @@ void main(void) {
 	// 매개변수가 없는 생성자를 호출
 	// Student juyoung = Student();
 	Student juyoung = Student(2115, "윤주영");
+	Student jwp = Student();
 
 }
