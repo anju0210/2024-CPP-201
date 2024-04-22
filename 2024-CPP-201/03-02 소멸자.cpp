583b7c68ed0cff952cca167caacb5bf42c3e710d
anju0210 <s2309@e-mirim.hs.kr>
2024-04-22 14:39:49 +0900

    03-02 생성자, 소멸자 호출시기 확인

--- 2024-CPP-201/03-02 소멸자.cpp
+++ 2024-CPP-201/03-02 소멸자.cpp
@@ -10,6 +10,7 @@ public:
 		this->hakbun_ = hakbun;
 		name_ = new char[18];	// 동적할당
 		strcpy(name_, name);	// 동적할당된 위치에 "윤주영"(name)을 복사한다
+		cout << "생성자 호출 완료" << endl;
 	}
 
 	Student(){ }
@@ -18,6 +19,7 @@ public:
 	// 소멸자 : 객체가 사라질 때(메모리 공간이 해제될 때) 호출되는 함수
 	~Student() {
 		delete []name_;
+		cout << "소멸자 호출 완료" << endl;
 	}
 
 	void print() {
@@ -33,7 +35,9 @@ private:
 void main(void) {
 
 	Student* stu = new Student(2115, (char*)"윤주영");
-	stu->print();
+	cout << "사필규정" << endl;
 
 	delete stu;
+	cout << "고진감래" << endl;
+
 }
