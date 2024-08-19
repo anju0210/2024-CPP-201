748b91941ff96e67b8e2fe9e3f5afb3fefedcdca
anju0210 <s2309@e-mirim.hs.kr>
2024-08-19 13:48:11 +0900

    동적할당 복습

---
+++ 2024-CPP-201/05-02 다형성.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal {
+public:
+	void bark() {
+		cout << "짖는다" << endl;
+	}
+
+	void sleep() {
+		cout << "잘 잔다" << endl;
+	}
+
+	void eat() {
+		cout << "잘 먹는다" << endl;
+	}
+
+private:
+	string name;
+	unsigned int age;
+};
+
+void main(void) {
+	Animal* ani = new Animal();
+	ani->bark();
+	ani->sleep();
+	ani->eat();
+
+	delete ani;
+}
