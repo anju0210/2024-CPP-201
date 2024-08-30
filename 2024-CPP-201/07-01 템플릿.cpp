d7fb3dd8c33d980007dbd0224465627f2a448b24
anju0210 <s2309@e-mirim.hs.kr>
2024-08-30 09:14:30 +0900

    07-01 템플릿 적용 후

--- 2024-CPP-201/07-01 템플릿.cpp
+++ 2024-CPP-201/07-01 템플릿.cpp
@@ -3,15 +3,11 @@
 
 using namespace std;
 
-int sum(int a, int b) {
+template<typename T>
+T sum(T a, T b) {
 	return a + b;
 }
-float sum(float a, float b) {
-	return a + b;
-}
-string sum(string a, string b) {
-	return a + b;
-}
+
 void main(void) {
 	cout << sum(1, 2) << endl;
 	cout << sum(1.f, 2.f) << endl;
