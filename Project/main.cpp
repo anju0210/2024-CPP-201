19cbc81e5371a4a7f05558060b05a3e21436ba51
anju0210 <s2309@e-mirim.hs.kr>
2024-10-21 14:44:25 +0900

    [pj] 글자 한글화

--- Project/main.cpp
+++ Project/main.cpp
@@ -11,6 +11,23 @@ enum App {
 
 void main(void) {
 	RenderWindow window(VideoMode(App::WIDTH, App::HEIGHT), "Project");
+	// 초당 프레임 조절(컴퓨터 성능과 상관없이 일정한 속도)
+	window.setFramerateLimit(60);
+
+	RectangleShape player;
+	player.setSize(Vector2f(200.f, 200.f));
+	player.setPosition((App::WIDTH - player.getSize().x) / 2, (App::HEIGHT - player.getSize().y) / 2);
+	player.setFillColor(Color::Magenta);
+
+	Font font;
+	font.loadFromFile("gulim.ttc");
+
+	Text text;
+	text.setFont(font);
+	text.setCharacterSize(50);
+	text.setString(L"굴림");
+	text.setFillColor(Color::Red);
+	text.setPosition((App::WIDTH - (text.getGlobalBounds().width))/1, 0.f);
 
 
 	while (window.isOpen()) {
@@ -20,10 +37,6 @@ void main(void) {
 				window.close();
 		}
 
-		RectangleShape player;
-		player.setSize(Vector2f(200.f, 200.f));
-		player.setPosition((App::WIDTH - player.getSize().x) / 2, (App::HEIGHT - player.getSize().y)/2);
-		player.setFillColor(Color::Magenta);
 
 		if (Keyboard::isKeyPressed(Keyboard::Key::Right))
 			player.move(10.f, 0.f);
@@ -36,6 +49,7 @@ void main(void) {
 
 		window.clear(Color::White);
 		window.draw(player);
+		window.draw(text);
 		window.display();
 
 	}
